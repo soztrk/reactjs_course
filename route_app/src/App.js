18747a2a332fb654bde810d370470b6fa978f493
S.Öztürk <s.ozturk999@gmail.com>
2022-07-06 12:13:59 +0300

    Route App : Completed

--- route_app/src/App.js
+++ route_app/src/App.js
@@ -1,8 +1,30 @@
-function App() {
+import {Route,Switch,Redirect} from "react-router-dom"
+import Welcome from "./pages/Welcome"
+import Products from "./pages/Products"
+import Header from "./components/Header"
+import ProductDetail from "./pages/ProductDetail"
+
+const App = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
