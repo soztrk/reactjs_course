9a8516583d8fadfd158f935256315fdf315fc8f1
S.Öztürk <s.ozturk999@gmail.com>
2022-07-07 11:40:04 +0300

    Quote App : Firebase deploy

--- quotes_app/src/App.js
+++ quotes_app/src/App.js
@@ -1,30 +1,36 @@
+import React,{Suspense} from "react"
 import {Redirect, Route,Switch} from "react-router-dom"
-import NewQuote from "./pages/NewQuote"
-import QuotesDetail from "./pages/QuoteDetail"
-import Quotes from "./pages/Quotes"
+
 import Layout from "./components/layout/Layout"
-import NotFound from "./pages/NotFound"
+import LoadingSpinner from "./components/UI/LoadingSpinner"
+
+const NewQuote = React.lazy(()=>import('./pages/NewQuote'))
+const QuotesDetail = React.lazy(()=>import('./pages/QuoteDetail'))
+const NotFound = React.lazy(()=>import('./pages/NotFound'))
+const Quotes = React.lazy(()=>import('./pages/Quotes'))
 
 const App = () => {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <Quotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuotesDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <Quotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuotesDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
